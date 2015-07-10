4b5cada2339a02f30ac290be8fa1384755ec0cce
Rodrigo <rodrigogsb14@hotmail.com>
2015-07-10 13:32:48 -0400

    Create PortfolioService.java

---
+++ PortfolioService.java
@@ -0,0 +1,21 @@
+
+JSON = 
+
+[ 
+  {
+    "id": 1,
+    "name" : "Portfolio for my career as an professional dota player",
+    "style": "style",
+    "assetNumber":4,
+    "lastModify": XXX,
+    "student":{
+                studentId:1
+                studentName: "Rodrigo Santos"
+                studentProfilePicture: url
+              }
+  },
+  {
+    .....
+  }........
+        
+]
