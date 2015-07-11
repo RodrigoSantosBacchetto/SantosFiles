866b4e55e9555c73dbae9e8afa4b88efddf7bd0a
Rodrigo <rodrigogsb14@hotmail.com>
2015-07-11 10:39:01 -0400

    Create PortfolioGatewayController.java

---
+++ PortfolioGatewayController.java
@@ -0,0 +1,19 @@
+
+
+getPortfolioSharedPeople(portfolioId)
+
+return  [
+          {
+          "userId":1
+          "name":"pepito"
+          "apellido":"pepe"
+          },
+          {
+          "userId":2
+          "name":"pepito2"
+          "apellido":"pepe2"
+          },
+          {},
+          ....
+        ]
+        
