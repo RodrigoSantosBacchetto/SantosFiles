12b3b183389271a6d13da511aed2d8bb15c6f913
Rodrigo <rodrigogsb14@hotmail.com>
2015-07-11 10:42:58 -0400

    Update PortfolioGatewayController.java

--- PortfolioGatewayController.java
+++ PortfolioGatewayController.java
@@ -17,3 +17,36 @@ return  [
           ....
         ]
         
+setPortfolioSharedPeople(portfolioId,List<Integer>(una lista de todos los userId),"appuser"(un String))
+
+evaluatorsForStudentEnrrolled()
+return [
+          {
+          "userId":1
+          "name":"pepito"
+          "apellido":"pepe"
+          },
+          {
+          "userId":2
+          "name":"pepito2"
+          "apellido":"pepe2"
+          },
+          {},
+          ....
+        ]
+
+evaluatorsThatMatchString("String"(alguna cadena con la que se busca concordancia de nombre o apellido))
+return [
+          {
+          "userId":1
+          "name":"pepito"
+          "apellido":"pepe"
+          },
+          {
+          "userId":2
+          "name":"pepito2"
+          "apellido":"pepe2"
+          },
+          {},
+          ....
+        ]
