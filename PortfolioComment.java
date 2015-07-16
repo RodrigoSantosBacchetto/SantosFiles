424829cc7ec162ce9958c77b6ed916d164b948f8
Rodrigo <rodrigogsb14@hotmail.com>
2015-07-16 16:30:26 -0400

    Create PortfolioComment.java

---
+++ PortfolioComment.java
@@ -0,0 +1,16 @@
+
+getportfolioOrAssetComments( "portfolioId" || "assetId")
+
+return  "comments" : [
+                        {
+                          "userName" : "elumenUser"
+                          "comment" : "this is awesome"
+                          "date" : "07/07/07"
+                        },
+                        {
+                          "userName" : "StudentUser"
+                          "comment" : "this is awesome"
+                          "date" : "07/07/07"
+                        },
+                        .....
+                     ]
