6837a16c8708f9708f27d8e3958f29258772c4ca
Rodrigo <rodrigogsb14@hotmail.com>
2015-07-16 16:31:17 -0400

    Update PortfolioComment.java

--- PortfolioComment.java
+++ PortfolioComment.java
@@ -10,7 +10,8 @@ return  "comments" : [
                         {
                           "userName" : "StudentUser"
                           "comment" : "this is awesome"
-                          "date" : "07/07/07"
+                          "date" : "08/08/08"
                         },
                         .....
                      ]
+//Lista de mapas ordenadas por fecha
