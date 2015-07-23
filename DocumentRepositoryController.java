1f6e78054c2099cd75be0fcd247bad201e7a6e66
Rodrigo <rodrigogsb14@hotmail.com>
2015-07-23 16:14:43 -0400

    Create DocumentRepositoryController.java

---
+++ DocumentRepositoryController.java
@@ -0,0 +1,21 @@
+
+/**
+* This method get all the files and folders on a specific folder
+*/
+getAllDataOnFolder("folderId")
+return [
+          {
+            "id" : 1
+            "type" : "folder"
+            "lastModification" : "21/12/12"
+            "isFile" : true
+            if(isFile==true){
+              "artifactName" : "xxx.mp4"
+              "artifactPath" : "/amazon/elumen/xxx.mp4"
+            }else{
+              "folderName" : "folder"
+            }
+          },
+          {},
+          {}..
+        ]
