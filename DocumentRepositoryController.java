aa7df55ffcf6d660e0602824dafb66f1f60ef13e
Rodrigo <rodrigogsb14@hotmail.com>
2015-07-24 11:19:38 -0400

    Update DocumentRepositoryController.java

--- DocumentRepositoryController.java
+++ DocumentRepositoryController.java
@@ -19,3 +19,29 @@ return [
           {},
           {}..
         ]
+        
+        
+/**
+* This method move the file or folder into a specific folder
+*/
+moveFileOrFolder("folderId"(target folder),"fileOrFolder"(File or folder that we want to move))
+return 
+          if(canMove){
+              "doneMsg", "Moved successfully"
+          }else{
+               "errorMsg","Error trying to move the folder or file"   
+          }
+          
+          
+/**
+* This method delete the file or folder 
+*/
+deleteFileOrFolder("fileOrFolder"(File or folder that we want to delete))
+return 
+          if(canDelete{
+              "doneMsg", "Deleted successfully"
+          }else{
+               "errorMsg","Error trying to delete the folder or file"   
+          }        
+          
+      
